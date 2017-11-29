e222f188c1e8416e53ec0969b63168dccf8d7de9
sarang <sarang@sarang-Inspiron-3521>
2017-11-29 17:51:36 +0530

    UPDATE IN CASTE CATEGORY PAGE

---
+++ SMGMT/src/com/servletStore/settings/caste/model/CasteDAO.java
@@ -0,0 +1,22 @@
+package com.servletStore.settings.caste.model;
+
+import java.util.List;
+
+public interface CasteDAO {
+	
+	//CasteCategory
+	public void addCasteCategory(CastePOJO castePojo);
+	
+	public List<CastePOJO> getCategoryDetails();
+	
+	public void deleteCategory(int category_id);
+	
+	
+	//Religion
+	public void addReligion(CastePOJO castePojo);
+	
+	public List<CastePOJO> getReligionDetails();
+	
+	public void deleteReligion(int religion_id);
+
+}
