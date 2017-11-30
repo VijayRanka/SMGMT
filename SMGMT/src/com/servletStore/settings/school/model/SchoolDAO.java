1df3bf7e95da61c3fa348d068b604087bdc1a272
sandeep <sandeep@sandeep-PC>
2017-11-30 15:54:42 +0530

    smgmt updated

--- SMGMT/src/com/servletStore/settings/school/model/SchoolDAO.java
+++ SMGMT/src/com/servletStore/settings/school/model/SchoolDAO.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 public interface SchoolDAO {
 	
-	public void addSchool(SchoolPOJO schoolPojo);
+	public void addSchool(SchoolPOJO schoolPojo,int len);
 	public List<SchoolPOJO> getSchoolInfo();
 	public List<SchoolPOJO> getSchoolDetails();
+	public void updateSchoolDetails(SchoolPOJO schoolPojo,String schoolId);
+	public List<SchoolPOJO> getSection(SchoolPOJO schoolPojo);
+	public List<SchoolPOJO> selectSchoolDetails(SchoolPOJO schoolPojo,String id);
+	
 	
 }
